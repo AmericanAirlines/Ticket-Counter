f5b8c3da8ad44c92b225107d47fd52eababe71c5
Spencer Kaiser <spencer.kaiser@aa.com>
2021-02-13 13:59:42 -0600

    Fixing bugs

--- src/slack/events/messageReplied.ts
+++ src/slack/events/messageReplied.ts
@@ -4,17 +4,17 @@ import { env } from '../../env';
 import logger from '../../logger';
 import { AppMiddlewareFunction } from '../types';
 
-let appId: string;
+let appUserId: string;
 
 export const messageReplied: AppMiddlewareFunction<SlackEventMiddlewareArgs<'message'>> = (app: App) => async ({
   message,
 }) => {
   const { parent_user_id: parentUserId, text, ts, channel } = message as GenericMessageEvent;
 
-  appId = appId ?? ((await app.client.bots.info({ token: env.slackBotToken })).bot as any)?.user_id;
-  if (parentUserId !== appId || message.subtype === 'bot_message') {
-    // The parent message is not something the bot wrote; ignore the thread reply
-    // OR the reply is from a bot
+  appUserId = appUserId ?? ((await app.client.auth.test({ token: env.slackBotToken })) as any).user_id;
+  if (parentUserId !== appUserId || message.subtype === 'bot_message') {
+    // The parent message is not something the bot wrote OR the reply is from a bot
+    // -> ignore the event entirely
     return;
   }
 
