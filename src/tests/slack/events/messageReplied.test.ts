cdb10919fe7a3cc7ce40fd8f77acb76fc8a8e476
Spencer Kaiser <spencer.kaiser@aa.com>
2021-02-23 10:13:39 -0600

    Adding more test coverage

--- src/tests/slack/events/messageReplied.test.ts
+++ src/tests/slack/events/messageReplied.test.ts
@@ -2,13 +2,14 @@ import 'jest';
 import { AllMiddlewareArgs, App, Middleware, SlackEventMiddlewareArgs } from '@slack/bolt';
 import logger from '../../../logger';
 
-jest.spyOn(logger, 'info').mockImplementation();
+const loggerInfoSpy = jest.spyOn(logger, 'info').mockImplementation();
 const loggerErrorSpy = jest.spyOn(logger, 'error').mockImplementation();
 jest.spyOn(logger, 'debug').mockImplementation();
 jest.mock('../../../env');
 
+const postMessageMock = jest.fn();
 jest.mock('../../../github/utils/postMessage.ts', () => ({
-  postMessage: jest.fn(),
+  postMessage: postMessageMock,
 }));
 
 const mockTs = '123456.789';
@@ -41,23 +42,32 @@ const mockApp = {
 };
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const ticketFindOneMock = jest.fn((_args: any) => ({
-  platformPostId: mockTs,
-  issueId: 'KJFAKSJFKAHLKF',
-}));
+const ticketFindOneMock = jest.fn(
+  async (): Promise<any> => ({
+    platformPostId: mockTs,
+    issueId: 'KJFAKSJFKAHLKF',
+  }),
+);
 jest.mock('../../../entities/Ticket.ts', () => {
   const { Platform } = jest.requireActual('../../../entities/Ticket.ts');
   return {
     Ticket: {
-      findOne: jest.fn((args) => ticketFindOneMock(args)),
+      findOne: ticketFindOneMock,
     },
     Platform,
   };
 });
 
-function getMockMessageEvent(parentUserId: string, text: string, ts: string, channel: string, subtype?: string) {
+function getMockMessageEvent(
+  parentUserId: string,
+  text: string | undefined,
+  ts: string,
+  channel: string,
+  subtype?: string,
+  files?: string[],
+) {
   return ({
-    message: { parent_user_id: parentUserId, text, ts, channel, subtype },
+    message: { parent_user_id: parentUserId, text, ts, channel, subtype, files },
   } as unknown) as SlackEventMiddlewareArgs<'message'> & AllMiddlewareArgs;
 }
 
@@ -83,6 +93,7 @@ describe('messageReplied event listener', () => {
     expect(authTestMock).toBeCalled();
     expect(reactionsAddMock).toBeCalled();
     expect(usersInfoMock).toBeCalled();
+    expect(postMessageMock).toBeCalledTimes(1);
     const { timestamp, channel, name } = reactionsAddMock.mock.calls[0][0];
     expect(timestamp).toEqual(mockTs);
     expect(channel).toEqual(mockChannel);
@@ -123,5 +134,39 @@ describe('messageReplied event listener', () => {
     await messageRepliedHandler(mockMessageEvent);
 
     expect(reactionsAddMock).not.toBeCalled();
+  });
+
+  it('defaults to an error text if no message is found', async () => {
+    const mockMessageEvent = getMockMessageEvent(mockAppId, undefined, mockTs, mockChannel);
+    await messageRepliedHandler(mockMessageEvent);
+
+    expect(postMessageMock).toBeCalledTimes(1);
+    const { message } = postMessageMock.mock.calls[0][1];
+    expect(message).toEqual('Could not load message, please see this ticket in Slack');
+  });
+
+  it('includes a link to the message if file(s) are included', async () => {
+    const mockMessageEvent = getMockMessageEvent(
+      mockAppId,
+      'This message has a file!',
+      mockTs,
+      mockChannel,
+      undefined,
+      ['some file URL'],
+    );
+    await messageRepliedHandler(mockMessageEvent);
+
+    expect(postMessageMock).toBeCalledTimes(1);
+    const { message } = postMessageMock.mock.calls[0][1];
+    expect(message).toContain('Message contains file(s), see Slack to view them');
+  });
+
+  it('logs info and does not respond if an associated ticket cannot be found', async () => {
+    const mockMessageEvent = getMockMessageEvent(mockAppId, 'This message has a file!', mockTs, mockChannel);
+    ticketFindOneMock.mockResolvedValueOnce(undefined);
+    await messageRepliedHandler(mockMessageEvent);
+
+    expect(postMessageMock).not.toBeCalled();
+    expect(loggerInfoSpy).toBeCalled();
   });
 });
