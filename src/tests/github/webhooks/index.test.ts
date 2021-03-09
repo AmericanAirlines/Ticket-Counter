61046fe1cb04d063ef34028d59fb0c729de37db3
John Kahn <7807353+johnkahn@users.noreply.github.com>
2021-03-09 10:40:31 -0600

    Test coverage for webhooks index.ts

---
+++ src/tests/github/webhooks/index.test.ts
@@ -0,0 +1,47 @@
+import { Webhooks } from '@octokit/webhooks';
+import logger from '../../../logger';
+import { env } from '../../../env';
+
+const errorLogger = jest.spyOn(logger, 'error').mockImplementation();
+
+jest.mock('../../../env', () => ({
+  env: {
+    githubAppWebhookSecret: 'GITHUB_APP_WEBHOOK_SECRET',
+  },
+}));
+
+const WebhooksMock = Webhooks as jest.Mock;
+const webhooksOnErrorMock = jest.fn();
+jest.mock('@octokit/webhooks', () => ({
+  Webhooks: jest.fn(() => ({
+    onError: jest.fn((...args) => webhooksOnErrorMock(...args)),
+  })),
+}));
+
+jest.mock('../../../github/webhooks/issueAssignmentChanged', () => ({ issueAssignmentChanged: jest.fn() }));
+jest.mock('../../../github/webhooks/issueClosed', () => ({ issueClosed: jest.fn() }));
+jest.mock('../../../github/webhooks/issueCommentedOn', () => ({ issueCommentedOn: jest.fn() }));
+jest.mock('../../../github/webhooks/issueReopened', () => ({ issueReopened: jest.fn() }));
+
+describe('GitHub Webhooks', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    jest.isolateModules(() => {
+      require('../../../github/webhooks');
+    });
+  });
+
+  it('Sets up Webhooks with secret from environment variable', () => {
+    expect(WebhooksMock).toHaveBeenCalledWith({
+      secret: env.githubAppWebhookSecret,
+    });
+  });
+
+  it('Logs to console, with error level, when there is a webhook error', () => {
+    const webhookError = new Error('WEBHOOK_ERROR');
+    webhooksOnErrorMock.mock.calls[0][0](webhookError);
+
+    expect(errorLogger).toHaveBeenCalledWith('Error handling GitHub webhook', webhookError);
+  });
+});
